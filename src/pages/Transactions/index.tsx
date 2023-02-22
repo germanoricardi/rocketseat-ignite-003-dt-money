bf317e25e8a293aaadc18b5d87c11dd7682e252f
Germano Ricardi <germanoricardi7@gmail.com>
2023-02-22 13:29:32 -0400

    Criado componente tabela de transações

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,5 +1,6 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
 
 export function Transactions() {
   return (
@@ -7,6 +8,37 @@ export function Transactions() {
       <Header />
       
       <Summary />
+
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td>Desenvolvimento de site</td>
+              <td>
+                <PriceHighlight variant="income">R$ 12.000,00</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>22/02/2023</td>
+            </tr>
+            <tr>
+              <td>Almoço</td>
+              <td>
+                <PriceHighlight variant="outcome">- R$ 59,00</PriceHighlight>
+              </td>
+              <td>Alimentação</td>
+              <td>22/02/2023</td>
+            </tr>
+            <tr>
+              <td>Computador</td>
+              <td>
+                <PriceHighlight variant="income">R$ 5.400,00</PriceHighlight>
+              </td>
+              <td>Computador</td>
+              <td>22/02/2023</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </div>
   )
 }
