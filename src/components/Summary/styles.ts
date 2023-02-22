95f2aff7a6eddfaa7aa96a3ea4ad5f0b5a179daa
Germano Ricardi <germanoricardi7@gmail.com>
2023-02-22 12:53:46 -0400

    Criado componente Summary;

---
+++ src/components/Summary/styles.ts
@@ -0,0 +1,41 @@
+import styled, { css } from "styled-components";
+
+export const SummaryContainer = styled.section`
+  margin: 0 auto;
+  max-width: 1120px;
+  padding: 0 1.5rem;
+  width: 100%;
+
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 2rem;
+
+  margin-top: -5rem;
+`
+
+interface SummaryCardProps {
+  variant?: 'green'
+}
+
+export const SummaryCard = styled.div<SummaryCardProps>`
+  background: ${props => props.theme["gray-600"]};
+  border-radius: 6px;
+  padding: 2rem;
+
+  header {
+    align-items: center;
+    color: ${props => props.theme["gray-300"]};
+    display: flex;
+    justify-content: space-between;
+  }
+
+  strong {
+    display: block;
+    font-size: 2rem;
+    margin-top: 1rem;
+  }
+
+  ${props => props.variant == 'green' && css`
+    background: ${props.theme["green-700"]};
+  `}
+`
