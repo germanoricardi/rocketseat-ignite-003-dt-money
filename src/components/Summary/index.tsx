0d6fe85152233ae82190307e2edd4ffe4bb1469e
Germano Ricardi <germanoricardi7@gmail.com>
2023-03-13 22:34:40 -0400

    Calculando resumo das Transactions com Reduce do javascript;

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -7,6 +7,24 @@ export function Summary() {
 
   const { transactions } = useContext(TransactionContext);
 
+  const summary = transactions.reduce(
+    (acc, transaction) => {
+      if(transaction.type == 'income') {
+        acc.income += transaction.price;
+        acc.total += transaction.price;
+      } else {
+        acc.outcome += transaction.price;
+        acc.total -= transaction.price;
+      }
+
+      return acc;
+    }, {
+      income: 0,
+      outcome: 0,
+      total: 0
+    }
+  )
+
   return(
     <SummaryContainer>
 
@@ -16,7 +34,7 @@ export function Summary() {
           <ArrowCircleUp size={32} color="#00B37E" />
         </header>
 
-        <strong>R$ 17.400,00</strong>
+        <strong>R$ {summary.income}</strong>
       </SummaryCard>
 
       <SummaryCard>
@@ -25,7 +43,7 @@ export function Summary() {
           <ArrowCircleDown size={32} color="#F75A68" />
         </header>
 
-        <strong>R$ 17.400,00</strong>
+        <strong>R$ {summary.outcome}</strong>
       </SummaryCard>
 
       <SummaryCard variant={'green'}>
@@ -34,7 +52,7 @@ export function Summary() {
           <CurrencyCircleDollar size={32} color="#FFFFFF" />
         </header>
 
-        <strong>R$ 17.400,00</strong>
+        <strong>R$ {summary.total}</strong>
       </SummaryCard>
 
     </SummaryContainer>
